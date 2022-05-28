cc9f42433c4ce511c498d13d90943ccf6f88c9d6
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-05-28 17:02:00 +0530

    Basic Homepage design

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import image from "./img/agri1.png";
+import img from "./img/agri2.jpeg";
+import indoorplant from "./img/indoorplant.PNG"
+import potsplanters from "./img/potsplanters.PNG"
+import soilmanure from "./img/soilmanure.PNG"
+// import {StyleSheet} from "react"
+function Dashboard() {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">AgriTech</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Offers</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Plants
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a className="dropdown-item" href="#">Indoor Plants</a></li>
+              <li><a className="dropdown-item" href="#">Flowering Plants</a></li>
+            </ul>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Planters
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a className="dropdown-item" href="#">Plastic Pots</a></li>
+              <li><a className="dropdown-item" href="#">Seedling Tray</a></li>
+              <li><a className="dropdown-item" href="#">Grow Bags for Plants</a></li>
+            </ul>
+          </li>
+        </ul>
+        <form className="d-flex">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+          <button className="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src={image} className="d-block w-100" alt="..."></img>
+    </div>
+    <div className="carousel-item">
+      <img src={img} className="d-block w-100" alt="..."></img>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div><br></br>
+<h1 className="text-center">We are here to inspire a world where every plant journey is a personal revolution of inner change and growth</h1>
+<br></br>
+<div className="text-center">
+<Link to="./Indoorplant">
+<img src={indoorplant} className="img-thumbnail" alt="..."></img>
+</Link>
+<Link to="./Indoorplant">
+<img src={potsplanters} className="img-thumbnail" alt="..."></img>
+</Link>
+<Link to="./Indoorplant">
+<img src={soilmanure} className="img-thumbnail" alt="..."></img>
+</Link>
+</div>
+</div>
+  )
+}
+
+export default Dashboard;
+
+// const styles = StyleSheet.create({
+//   container:{
+//     justifyContent: "flex-start",
+//     marginRight: 10,
+//   }
+// })
--- server/index.js
+++ server/index.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const axios = require('axios');
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-//mongodb Connection
-mongoose.connect('mongodb+srv://suraj:suraj@cluster0.gfb4i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+ //mongodb Connection
+//  mongoose.connect('mongodb+srv://suraj:suraj@cluster0.gfb4i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+//      useNewUrlParser: true,
+//      useUnifiedTopology: true
+//  },()=>{
+//          console.log('Connected to mongodb');
+//  });
+ mongoose.connect('process.env.MONGO_DB_URL',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 },()=>{
-    console.log('Connected to mongodb');
+        console.log('Connected to mongodb');
 });
+
+
+// mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+//     console.log('Connected to DB 📦');
+// });
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+  
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+//     });
+// }
 
 
 const PORT = 5000;
