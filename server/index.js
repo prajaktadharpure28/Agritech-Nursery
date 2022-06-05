f26adba1c65b7cfcc4cd43d6d03b6660bd1c0097
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-06-05 17:14:43 +0530

    minor changes in index.js

--- server/index.js
+++ server/index.js
@@ -70,6 +70,14 @@ app.post('/update/plant', async (req, res) => {
 // mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
 //     console.log('Connected to DB 📦');
 // });
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+  
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+    });
+}
+
 // if(process.env.NODE_ENV === 'production') {
 //     app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
   
@@ -77,8 +85,6 @@ app.post('/update/plant', async (req, res) => {
 //         res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
 //     });
 // }
-
-
 const PORT = 5000;
 
 app.listen(PORT, () => {
