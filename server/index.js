920598f34980feca250f2529900a3dbb3148f913
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-06-05 20:12:10 +0530

    minor changes in index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 // const axios = require('axios');
 const Plant = require('./models/Plant')
 const app = express();
+const path = require('path');
 require('dotenv').config()
 app.use(cors());
 app.use(express.json());
